56db5b8bb157c1fa8e277046519cdbdb440f3795
JoaoPauloCSA <jp10alencar@gmail.com>
2025-04-23 07:54:35 -0300

    Add files via upload

---
+++ POO2/AreaQuadrado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO2
+{
+    class AreaQuadrado
+    {
+        private double aresta;
+        private double resultado;
+
+        public AreaQuadrado()
+        {
+            this.aresta = 0;
+        }
+
+        public AreaQuadrado(double x)
+        {
+            this.aresta = x;
+        }
+
+        public void setAresta(double x)
+        {
+            this.aresta = x;
+        }
+
+        public double getAresta()
+        {
+            return this.aresta;
+        }
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+
+        public void calcular()
+        {
+            this.resultado = Math.Pow(this.aresta, 2);
+        }
+    }
+}
